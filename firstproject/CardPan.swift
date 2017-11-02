cdfc2a28db2097b5fd3d84d7b50c2f06e662bec9
Aravind Subramanian <aravind@AravindsMacBookPro.local>
2017-11-02 10:52:30 -0400

    First commit.

---
+++ firstproject/CardPan.swift
@@ -0,0 +1,14 @@
+//
+//  CardPan.swift
+//  firstproject
+//
+//  Created by Aravind Subramanian on 11/2/17.
+//  Copyright © 2017 Aravind Subramanian. All rights reserved.
+//
+
+import UIKit
+
+class CardPan: UIViewController{
+    
+    
+}
